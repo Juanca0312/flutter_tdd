9ee4df119c934437be848ef56c35324dda92185f
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2023-12-01 14:39:36 -0500

    Implemented NumberTrivia model fromJson with test

---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,12 @@
+import 'package:clean_tdd_hello_world/features/number_trivia/domain/entities/number_trivia.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  NumberTriviaModel({
+    required String text,
+    required int number,
+  }) : super(text: text, number: number);
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(text: json["text"], number: json["number"]);
+  }
+}
