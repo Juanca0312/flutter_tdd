4cf900b01f694d96634d93e231b4a2c61d116426
Juan Carlos Hernández <jchernandez0312@gmail.com>
2023-12-08 11:55:41 -0500

    episode 5 - contracts of data sources

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -1,5 +1,13 @@
 import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {
-  Failure([List properties = const <dynamic>[]]) : super();
+  Failure([List properties = const <dynamic>[]]);
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
 }
+// General failures
+class ServerFailure extends Failure {}
+
+class CacheFailure extends Failure {}
